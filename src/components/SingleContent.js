2089d46d39e75d48bd6762333b3bfaf20050e412
Thanaporn Sae-pueng <78015751+ploy-thanaporn@users.noreply.github.com>
2022-06-11 17:02:49 +0700

    update css

---
+++ src/components/SingleContent.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+
+const SingleContent = ({ title, description }) => {
+  // useState(false) ซ่อนเนื้อหา
+  const [showContent, setShowContent] = useState(false);
+  return (
+    <article className="content">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowContent(!showContent)}>
+          {showContent ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showContent && <p>{description}</p>}
+    </article>
+  );
+};
+
+export default SingleContent;
